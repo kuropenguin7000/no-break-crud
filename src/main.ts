6daaba6dca2a3e468a15eebbadbef75ea86461f6
pokopang <ctlveccc@gmail.com>
2025-07-04 21:21:35 +0700

    create register user API

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser("RAHASIA"));
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
+  app.useLogger(logger);
+  await app.listen(3000);
 }
 bootstrap();
--- src/user/user/user.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class UserService {
-
-  hello(name: string): string {
-    return `Hello ${name || 'Guest'}`;
-  }
-}
